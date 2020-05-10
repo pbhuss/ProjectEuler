1c1d6f473ae23a2935443fc52a961c90f37bb422
Peter Huss <pbhuss@users.noreply.github.com>
2020-05-10 12:46:23 -0700

    some testing improvements

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,17 @@
 import pkgutil
 import re
+from importlib import import_module
 
 import pytest
 import problems
 
 
 def load_solution_modules():
-    module_map = dict()
-    prefix = problems.__name__ + '.'
+    module_map = {}
+    prefix = f'{problems.__name__}.'
     for _, modname, _ in pkgutil.iter_modules(problems.__path__, prefix):
         problem_number = int(re.search(r'\d+', modname).group())
-        module_map[problem_number] = __import__(modname, fromlist=['foo'])
+        module_map[problem_number] = import_module(modname)
     return module_map
 
 
@@ -23,7 +24,8 @@ def pytest_addoption(parser):
         type='int',
         choices=solution_modules.keys(),
         default=solution_modules.keys(),
-        help='solution numbers to test (default: all)')
+        help='solution numbers to test (default: all)'
+    )
 
 
 def pytest_generate_tests(metafunc):
@@ -34,6 +36,6 @@ def pytest_generate_tests(metafunc):
     metafunc.parametrize('problem', metafunc.config.option.problems, ids=ids)
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def solution_modules():
     return load_solution_modules()
